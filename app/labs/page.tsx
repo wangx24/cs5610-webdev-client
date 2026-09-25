65f8050910fdace10364151fea869289f0c27d22
Xinyu Wang <215159159+wangx24@users.noreply.github.com>
2026-09-24 21:54:20 -0400

    add one more name

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -4,6 +4,7 @@ export default function Labs() {
   return (
     <div id="wd-labs">
       <h1>Labs</h1>
+      <h3>Xinyu Wang</h3>
       <ul>
         <li>
           <Link href="/labs/lab1">Lab 1: HTML Examples</Link>
